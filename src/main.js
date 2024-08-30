5b65fb638d44890e07ee7771a13581dc491f28c9
Artur Prepelytsia <preart81@gmail.com>
2024-08-30 20:53:56 +0300

    add header
    + responsive header
    + burger menu

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 // TODO listeners for actions
+
+// Header Burger menu
+const toggleButtons = document.querySelectorAll('.js-toggle-menu');
+const navMenu = document.querySelector('.js-header-menu');
+
+toggleButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const isExpanded = button.getAttribute('aria-expanded') === 'true';
+    toggleButtons.forEach(btn =>
+      btn.setAttribute('aria-expanded', !isExpanded)
+    );
+    navMenu.classList.toggle('open');
+  });
+});
+// Header Burger menu
