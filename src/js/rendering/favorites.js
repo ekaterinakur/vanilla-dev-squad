4f144d53e43705619a6ec9a74cbdd00b3317c29a
Artur Prepelytsia <preart81@gmail.com>
2024-08-31 16:50:50 +0300

    add favorites.js

---
+++ src/js/rendering/favorites.js
@@ -0,0 +1,8 @@
+// Header Importing event handlers
+import { toggleHeaderEventListeners } from '/js/eventHandlers/headerHandlers.js';
+
+// Header Setting up event listeners
+toggleHeaderEventListeners();
+
+// Header Listening for window resize events - hide burger menu on larger screens
+window.addEventListener('resize', toggleHeaderEventListeners);
