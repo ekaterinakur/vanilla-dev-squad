488017e55de353873dacc4dfe17f26c602475cc1
ekaterinakur <ekaterina1.kur@gmail.com>
2024-08-31 14:57:50 +0300

    Refactoring

--- src/js/eventHandlers/handleSubscription.js
+++ src/js/eventHandlers/handleSubscription.js
@@ -1,6 +1,8 @@
-
 import { setSubscription } from '../api/setSubscription.js';
-import { showErrorNotification, showNotification } from '../rendering/common.js';
+import {
+  showErrorNotification,
+  showNotification,
+} from '../rendering/common.js';
 
 const subscriptionForm = document.querySelector('.subscribe__form');
 const emailInput = subscriptionForm.querySelector('[name="email"]');
@@ -13,11 +15,11 @@ export async function handleSubmitSubscription(event) {
   try {
     const email = emailInput.value;
     const res = await setSubscription(email);
-		
-		showNotification(res.message);
+
+    showNotification(res.message);
 
     subscriptionForm.reset();
   } catch (error) {
-		showErrorNotification(error.message);
+    showErrorNotification(error.message);
   }
 }
