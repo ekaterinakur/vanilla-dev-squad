ac0e27863a0854fbda4ed1cc8469d9d329a9574d
preart81 <59435348+preart81@users.noreply.github.com>
2024-09-01 10:50:52 +0000

    Deploying to gh-pages from @ ekaterinakur/vanilla-dev-squad@48b4a3b6568b5c4296de3d48e45fccd3ebf34e50 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import{t as e}from"./assets/headerHandlers-eab94a71.js";window.addEventListener("resize",()=>{e()});e();
+import{t as e}from"./assets/headerHandlers-1a04d509.js";window.addEventListener("resize",()=>{e()});e();
 //# sourceMappingURL=commonHelpers.js.map
