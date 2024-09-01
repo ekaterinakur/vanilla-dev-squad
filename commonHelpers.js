c9de8e2be3e038ac40f7d7169356cf0bd08027cd
preart81 <59435348+preart81@users.noreply.github.com>
2024-09-01 16:52:48 +0000

    Deploying to gh-pages from @ ekaterinakur/vanilla-dev-squad@ed4dac05e2a880b1a794587e50da0a1f46873070 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,24 +1,24 @@
-import"./assets/styles-2224ec19.js";let e=JSON.parse(localStorage.getItem("favorites")),s=document.querySelector(".favorites-container");const r="It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future",o=()=>{document.querySelector(".favorites-container-empty")&&document.querySelector(".favorites-container-empty").remove();const i=e.map(t=>`<li class="favorites-container-list-item">
+import{s as e}from"./assets/sprite-327f9b99.js";let s=JSON.parse(localStorage.getItem("favorites")),i=document.querySelector(".favorites-container");const r="It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future",a=()=>{document.querySelector(".favorites-container-empty")&&document.querySelector(".favorites-container-empty").remove();const o=s.map(t=>`<li class="favorites-container-list-item">
       <div class="list-item-header">
         <div class="list-item-header-text-wrapper">
           <p class="list-item-header-text">Workout</p>
           <button class="list-item-header-button-delete">
             <svg class="list-item-header-icon" width="16" height="16">
-              <use href="../img/sprite.svg#icon-trash"></use>
+              <use href=".${e}#icon-trash"></use>
             </svg>
           </button>
         </div>
         <button class="list-item-header-button-start">
           <p class="list-item-header-button-text">Start</p>
           <svg class="list-item-header-button-icon" width="13" height="13">
-            <use href="../img/sprite.svg#icon-arrow"></use>
+            <use href=".${e}#icon-arrow"></use>
           </svg>
         </button>
       </div>
       <div class="list-item-content">
         <div class="item-content-icon-wrapper">
           <svg class="item-content-icon" width="14" height="16">
-            <use href="../img/sprite.svg#icon-running-man"></use>
+            <use href=".${e}#icon-running-man"></use>
           </svg>
         </div>
         <p class="item-content-text">${t.name}</p>
@@ -34,5 +34,5 @@ import"./assets/styles-2224ec19.js";let e=JSON.parse(localStorage.getItem("favor
           Target:<span class="item-footer-text">${t.target}</span>
         </p>
       </div>
-    </li>`).join("");s.insertAdjacentHTML("beforeend",`<ul class="favorites-container-list">${i}</ul>`)},a=()=>{document.querySelector(".favorites-container-list")&&document.querySelector(".favorites-container-list").remove(),s.insertAdjacentHTML("beforeend",`<p class="favorites-container-empty">${r}</p>`)};e.length>0?o():a();
+    </li>`).join("");i.insertAdjacentHTML("beforeend",`<ul class="favorites-container-list">${o}</ul>`)},n=()=>{document.querySelector(".favorites-container-list")&&document.querySelector(".favorites-container-list").remove(),i.insertAdjacentHTML("beforeend",`<p class="favorites-container-empty">${r}</p>`)};s.length>0?a():n();
 //# sourceMappingURL=commonHelpers.js.map
