f561f4f2496e0d78bf221b2e02ff405b5698c5c7
oleksbod <oleks.bodnar@gmail.com>
2024-09-01 15:32:54 +0000

    Deploying to gh-pages from @ ekaterinakur/vanilla-dev-squad@dc6a36623611e7d32fb063ae36a83e6714b17a7c 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import{t as e}from"./assets/headerHandlers-0edf2b07.js";window.addEventListener("resize",()=>{e()});e();
+import{t as e}from"./assets/headerHandlers-6a6fdf84.js";window.addEventListener("resize",()=>{e()});e();
 //# sourceMappingURL=commonHelpers.js.map
